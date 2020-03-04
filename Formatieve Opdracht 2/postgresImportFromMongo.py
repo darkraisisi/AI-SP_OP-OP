82875a6900d41875506bdd9e249fe03fde3b50da
Niels <nbijl@petersplanet.nl>
2020-03-04 16:48:56 +0100

    commit insert into database

---
+++ Formatieve Opdracht 2/postgresImportFromMongo.py
@@ -0,0 +1,19 @@
+import psycopg2
+import random, os, json, urllib.parse, requests
+
+
+connection = psycopg2.connect(user="postgres",
+                              password="wachtwoord",
+                              host="localhost",
+                              port="5432",
+                              database="OpisOp")
+cursor = connection.cursor()
+sql = """INSERT INTO products (name)
+VALUES ('zeep');
+    """
+cursor.execute(sql)
+connection.commit()
+
+cursor.close()
+connection.close()
+print("PostgreSQL connection is closed")
