5223df9376b5aa23ee0d3e96ab8f30ecff6dc4cc
Fr1so <60092895+Fr1so@users.noreply.github.com>
2020-03-06 17:31:25 +0100

    help aaa

--- Formatieve Opdracht 2/fillDatabase.py
+++ Formatieve Opdracht 2/fillDatabase.py
@@ -8,10 +8,9 @@ connection = psycopg2.connect(user="postgres",
 cursor = connection.cursor()
 
 cursor.execute(
-            """INSERT INTO products VALUES (id,category,subcategory,subsubcategory,gender,name) ;
-            
-                FROM 'C:\\Users\\frisf\\Documents\\GitHub\\AI-SP_OP-OP\\Formatieve Opdracht 2\\products.csv' 
-                
+            """INSERT INTO products VALUES (id,category,subcategory,subsubcategory,gender,name)
+                FROM 'C:\\Users\\frisf\Documents\GitHub\AI-SP_OP-OP\Formatieve Opdracht 2\products.csv'
+                WITH DELIMITER ',' CSV HEADER
 """
 )
 
