28f626483705f82960c24686bfcaad1ac6bdcb21
Niels <nbijl@petersplanet.nl>
2020-03-06 13:57:50 +0100

    code van nick (uit de les)

--- Formatieve Opdracht 2/importDBtoCSV.py
+++ Formatieve Opdracht 2/importDBtoCSV.py
@@ -1,10 +1,21 @@
+from pymongo import MongoClient
+import csv
+
+envvals = ["MONGODBUSER", "MONGODBPASSWORD", "MONGODBSERVER", "RECOMADDRESS"]
+dbstring = 'mongodb+srv://{0}:{1}@{2}/test?retryWrites=true&w=majority'
+
+client = MongoClient()
+database = client.huwebshop
+
+products = database.products.find()
+
 print("Creating the product database contents...")
 with open('products.csv', 'w', newline='') as csvout:
     fieldnames = ['id', 'category', 'subcategory', 'subsubcategory']
     writer = csv.DictWriter(csvout, fieldnames=fieldnames)
     writer.writeheader()
     c = 0
-    for product in mongodatabase.products.find():
+    for product in products:
         writer.writerow({'id': product["_id"],
                          'category': product.get("category", None),
                          'subcategory': product.get("sub_category", None),
@@ -14,3 +25,7 @@ with open('products.csv', 'w', newline='') as csvout:
         if c % 10000 == 0:
             print("{} product records written...".format(c))
 print("Finished creating the product database contents.")\
+
+
+#           BRONVERMELDING
+#   Nick zijn code gebruikt uit de les
