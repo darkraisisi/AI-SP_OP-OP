2c7ab51b564b4c38054ed1dec97bbd619a5a277f
Niels <nbijl@petersplanet.nl>
2020-03-10 11:30:20 +0100

    importDBtoCSV functie van gemaakt en meer gestructureed gekregen

--- Formatieve Opdracht 2/importDBtoCSV.py
+++ Formatieve Opdracht 2/importDBtoCSV.py
@@ -12,77 +12,35 @@ sessions = database.sessions.find()
 profiles = database.profiles.find()
 
 
-"""
--- -----------------------------------------------------
--- Generate products.csv file
--- -----------------------------------------------------
-"""
+def generateCSV(fileNameString, category, fieldnames, values):
+    print("Creating the product database contents...")
+    with open(fileNameString, 'w', newline='', encoding='utf-8') as csvout:
+        writer = csv.DictWriter(csvout, fieldnames=fieldnames)
+        writer.writeheader()
+        c = 0
+        for record in category:
+            writeDict = {}
+            for x in range(len(fieldnames)):
+                if x == 0:
+                    writeDict.update({fieldnames[x]: record[values[0]]})
+                else:
+                    writeDict.update({fieldnames[x]: record.get(values[1], None)})
+            writer.writerow(writeDict)
+            c += 1
+            if c % 10000 == 0:
+                print("{} product records written...".format(c))
+    print("Finished creating the product database contents.")
 
-"""
--- -----------------------------------------------------
--- Generate products.csv file
--- -----------------------------------------------------
-"""
 
-print("Creating the product database contents...")
-with open('products.csv', 'w', newline='', encoding='utf-8') as csvout:
-    fieldnames = ['id', 'name', 'gender', 'category', 'subcategory', 'subsubcategory', 'brand']
-    writer = csv.DictWriter(csvout, fieldnames=fieldnames)
-    writer.writeheader()
-    c = 0
-    for product in products:
-        writer.writerow({'id': product["_id"],
-                         'name': product.get("name", None),
-                         'gender': product.get("gender", None),
-                         'category': product.get("category", None),
-                         'subcategory': product.get("sub_category", None),
-                         'subsubcategory': product.get("sub_sub_category", None),
-                         'brand': product.get("brand", None)
-                         })
-        c += 1
-        if c % 10000 == 0:
-            print("{} product records written...".format(c))
-print("Finished creating the product database contents.")\
-
-"""
--- -----------------------------------------------------
--- Generate sessions.csv file
--- -----------------------------------------------------
-"""
-print("Creating the sessions database contents...")
-with open('sessions.csv', 'w', newline='', encoding='utf-8') as csvout:
-    fieldnames = ['id', 'segment']
-    writer = csv.DictWriter(csvout, fieldnames=fieldnames)
-    writer.writeheader()
-    c = 0
-    for session in sessions:
-        writer.writerow({'id': session["_id"],
-                         'segment': session.get("segment", None)
-                         })
-        c += 1
-        if c % 10000 == 0:
-            print("{} product records written...".format(c))
-print("Finished creating the session database contents.")
-
-"""
--- -----------------------------------------------------
--- Generate profiles.csv file
--- -----------------------------------------------------
-"""
-print("Creating the profile database contents...")
-with open('profiles.csv', 'w', newline='', encoding='utf-8') as csvout:
-    fieldnames = ['id', 'browser_id']
-    writer = csv.DictWriter(csvout, fieldnames=fieldnames)
-    writer.writeheader()
-    c = 0
-    for profile in profiles:
-        writer.writerow({'id': profile["_id"],
-                         'browser_id': profile.get("buids", None)
-                         })
-        c += 1
-        if c % 10000 == 0:
-            print("{} product records written...".format(c))
-print("Finished creating the profile database contents.")
+generateCSV('products.csv', products,
+            ['id', 'name', 'gender', 'category', 'subcategory', 'subsubcategory', 'brand'],
+            ["_id", "name", "gender", "category", "sub_category", "sub_sub_category", "brand"])
+generateCSV('sessions.csv', sessions,
+            ['id', 'segment'],
+            ["_id", "segment"])
+generateCSV('profiles.csv', profiles,
+            ['id', 'browser_id'],
+            ["_id", "buids"])
 
 #           BRONVERMELDING
 #   Dit is de code die gebruikt is tijdens de les van 5 maart met Nick.
